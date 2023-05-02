c1a2279fcfe07e64c17bd0991dd5cb37107fbd55
Ursula Armstrong <cukimonkey@gmail.com>
2023-05-02 17:13:07 +0100

    added basic structure and review cardsto the Testimonial Component. Next stpes - styling Testimonial components

---
+++ src/Reviews.js
@@ -0,0 +1,61 @@
+import { Card as ChakraCard, CardFooter, Stack, HStack, Image, Heading, Text, Divider, Button as ChakraButton, ButtonGroup } from '@chakra-ui/react'
+
+import React from "react";
+import './App.css';
+
+function Reviews ({ name, imageSrc })  {
+return(
+  <ChakraCard className="CardContainer" 
+  style={{
+      backgroundColor: '#EDEFEE',
+      borderTopRightRadius: '16px',
+      borderTopLeftRadius: '16px',
+      paddingBottom: '20px',
+
+    }}>
+      
+    <Stack >
+        
+            <Heading className="ReviewHeader" style={{
+              fontSize: "24px", 
+              fontFamily: "Markazi Text",
+              marginTop: '20px',
+              marginLeft: '20px',
+              textAlign: 'start'
+              }}>Rating</Heading>
+    <HStack>
+        <Image className="ReviewImage"
+        src={imageSrc} 
+        style={{
+        height: '70px',
+        width: '70px'
+        }} 
+        alt='user'
+        />
+        <Text className="UserName" style={{
+          fontSize: "16px", 
+          fontFamily: 'Karla',
+          margin: '20px', 
+          textAlign: 'start'
+          }}>{name}</Text>
+    </HStack>
+        
+    </Stack>
+    <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='4'>
+      <ChakraButton style={{
+        border: 'none',
+        bottom: '5px',
+        paddingTop: '20px'
+      }}>
+        See review
+      </ChakraButton>
+    </ButtonGroup>
+  </CardFooter>
+
+  </ChakraCard>
+);
+
+}
+export default Reviews;
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -1,9 +1,59 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
 import React from "react";
+import Reviews from "./Reviews";
+import './App.css';
 
-function Testimonials() {
-    return (
-        <h1>Testimonials</h1>
-    )
-}
+const reviews = [
+    {
+        name: "John Smith",
+        getImageSrc: () => (process.env.PUBLIC_URL + "/john.jpg"),
+      },
+      {
+        name: "Erica Thompson",
+        getImageSrc: () => (process.env.PUBLIC_URL + "/erica.jpg"),
+      },
+      {
+        name: "Leo Stark",
+        getImageSrc: () => (process.env.PUBLIC_URL + "/leo.jpg"),
+      },
+      {
+        name: "Eva Reader",
+        getImageSrc: () => (process.env.PUBLIC_URL + "/eva.jpg"),
+      },
+]
+
+const Testimonials = () => {
+  return (
+    <Box paddingLeft= '10%'
+    paddingRight= '10%'
+    paddingTop= '50px'
+    paddingButtom= '50px'>
+      <Container >
+      <Flex minWidth='max-content' gap='2'>
+                <Box >
+                  <h1>Testimonials</h1>
+                </Box>
+       </Flex>
+       <Flex className="cards2"
+       display='grid'
+       gridTemplateColumns= '1fr 1fr 1fr'
+       gap='20'
+       paddingTop= '50px'
+
+        >
+          {reviews.map((reviews) => (
+
+            <Reviews
+                imageSrc={reviews.getImageSrc()}
+                key={reviews.title}
+                name={reviews.title}
+            />
+          ))}
+
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
 
 export default Testimonials;
